c52b3495d10e4bc3224b6b44692eb1da6c5281f2
Ayzrofi <45961870+Ayzrofi@users.noreply.github.com>
2018-12-20 16:39:13 +0700

    Initial commit

---
+++ Assets/_Scripts/HighScore.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HighScore : MonoBehaviour {
+
+    public Text HighScoreText;
+    void Start () {
+        HighScoreText.text = PlayerPrefs.GetInt("HighScore").ToString();
+	}
+}
